fff7605145541c3eced1ab8e9581fca46326f9df
pankaj-2702 <pankajkumar67920@gmail.com>
2025-08-27 13:47:35 +0530

    Update second.js

---
+++ second.js
@@ -0,0 +1,47 @@
+const messages = [
+  "📡 Connecting to NASA satellites...",
+  "📸 Analyzing your baby photos...",
+  "Checking mess menu impact on mental health...",
+  "Evaluating brain cells lost during viva...",
+  "Checking how many hours spent on reels...",
+  "Detecting how many crushes ghosted you...",
+  "Trying to load motivation.exe – failed.",
+  "Measuring caffeine levels in bloodstream..",
+  "📞 Calling your mom to confirm your birthdate...",
+  "⌛ Counting your wrinkles... please stay still.",
+  "🕺 Checking dance moves to estimate generation...",
+  "🧓 Asking old people if they remember you...",
+  "🗿 Carbon-dating your sense of humor...",
+  "🧃 Measuring how much juice you still have left...",
+  "📅 Reading ancient scrolls to find your birthday...",
+  "🐣 Checking if you're older than the internet...",
+  "🛸 Contacting aliens to verify your records...",
+  "🎮 Checking if you played Snake on Nokia...",
+  "💼 Evaluating number of job interviews survived...",
+  "🔦 Looking into your soul... it’s dusty.",
+  "💀 Making sure you're still breathing... just in case."
+];
+
+const femaleMsg =[
+    "You act all mature, but your smile exposes the teenage chaos you’re still hiding 😌",
+
+"Brains of a topper, but emotions of someone who still needs extra marks in attendance.",
+
+"You talk like a philosopher, but your heart still believes in Bollywood endings 💕",
+
+"On paper you’re grown… but your laugh still sounds like recess break.",
+
+"You’ve got the brain of a genius and the smile of a heart-stealer 😌",
+
+"Your maturity level is high… but you’re still cute enough to make anyone nervous."
+]
+
+const boysMsg =[
+    "You act like you’ve figured life out… then you trip over your own charger wire 😏",
+
+"Your maturity is CEO-level, but your crush-handling skills are still LKG standard.",
+
+"You flex wisdom, but still Google ‘symptoms of crush liking me back’ at 2 AM.",
+
+"You look like a man, but mentally you’re still negotiating with your mom for 5 more minutes of WiFi."
+]
